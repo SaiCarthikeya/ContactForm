bf41d35ff3b5b711597689bcac4ca726826ce834
SaiCarthikeya <saicarthik180@gmail.com>
2024-09-05 09:26:54 +0530

    forms

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const formSubmitted = (e)  => {
+        e.preventDefault();
+        console.log(name);
+        console.log(email);
+        console.log(message);
+    }
+    
     return (
         <div>
-            ContactForm
+            <form className="form-container" onSubmit={formSubmitted}>
+                <label htmlFor="name">Enter your name</label>
+                <input type="text" value={name} placeholder="name" className="input-field" 
+                onChange={(e) => setName(e.target.value)}/>
+                <label htmlFor="name">Enter your email</label>
+                <input type="text" value={email} placeholder="email" className="input-field" 
+                onChange={(e) => setEmail(e.target.value)}/>
+                <label htmlFor="name">Enter your message</label>
+                <textarea type="text" value={message} placeholder="Message" className="input-field" 
+                onChange={(e) => setMessage(e.target.value)}/>
+                <button type="submit" className="btn">Submit</button>
+            </form>
         </div>
     )
 }
